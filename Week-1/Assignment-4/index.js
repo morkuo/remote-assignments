970bd5c5b832dc3e3e8c335d8f24b3e084f1cac2
morkuo <80438073+morkuo@users.noreply.github.com>
2022-06-10 15:27:39 +0800

    submit

--- Week-1/Assignment-4/index.js
+++ Week-1/Assignment-4/index.js
@@ -1,5 +1,5 @@
 // rotate triangle icons
-const navItems = document.querySelectorAll("#items li.item");
+const navItems = document.querySelectorAll("#items li");
 navItems.forEach(list => {
   const triangleIcon = list.querySelector(".triangle_icon");
   if (triangleIcon) {
@@ -12,18 +12,9 @@ navItems.forEach(list => {
   }
 });
 
-const languageToggleBtns = document.querySelectorAll(".language_toggle_btn");
-languageToggleBtns.forEach(btn => {
-  const triangleIcon = btn.querySelector(".triangle_icon");
-  btn.addEventListener("mouseenter", () => {
-    triangleIcon.classList.toggle("triangle_icon_rotation");
-  });
-  btn.addEventListener("mouseleave", () => {
-    triangleIcon.classList.toggle("triangle_icon_rotation");
-  });
-});
-
+//Change color when the element is hovered over
 //language btns
+const languageToggleBtns = document.querySelectorAll(".language_toggle_btn");
 languageToggleBtns.forEach(btn => {
   btn.addEventListener("mouseenter", () => {
     btn.style.backgroundColor = "#f0f1f4";
@@ -67,6 +58,7 @@ searchBarClosePanel.addEventListener("click", () => {
 });
 
 /* extends items' menu when it is hovered over */
+//items
 navItems.forEach(list => {
   const triangleIcon = list.querySelector(".triangle_icon");
   if (triangleIcon) {
@@ -79,29 +71,83 @@ navItems.forEach(list => {
   }
 });
 
-//展開內容自己維持自己展開
+//language bar
+const navLanguageClosePanel = document.querySelector("#nav_language_close_panel");
+const footerLanguageClosePanel = document.querySelector("#footer_language_close_panel");
+languageToggleBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("item_extending");
+  });
+});
+
+navLanguageClosePanel.addEventListener("click", () => {
+  const extendingBtn = document.querySelector("#nav_language");
+  extendingBtn.classList.toggle("item_extending");
+  navLanguageClosePanel.classList.toggle("item_extending");
+});
+
+footerLanguageClosePanel.addEventListener("click", e => {
+  const extendingBtn = document.querySelector("#footer_language");
+  extendingBtn.classList.toggle("item_extending");
+  footerLanguageClosePanel.classList.toggle("item_extending");
+});
+
+//Hover展開內容自己維持自己展開
 //Product
 const itemProductDetail = document.querySelector("#item_product_detail");
 const itemProduct = itemProductDetail.previousElementSibling;
-const triangleIcon = itemProduct.querySelector("img");
+const itemProductTriangleIcon = itemProduct.querySelector("img");
 itemProductDetail.addEventListener("mouseenter", () => {
   itemProduct.classList.toggle("item_extending");
-  triangleIcon.classList.toggle("triangle_icon_rotation");
+  itemProductTriangleIcon.classList.toggle("triangle_icon_rotation");
 });
 itemProductDetail.addEventListener("mouseleave", () => {
   itemProduct.classList.toggle("item_extending");
-  triangleIcon.classList.toggle("triangle_icon_rotation");
+  itemProductTriangleIcon.classList.toggle("triangle_icon_rotation");
 });
 
-//moreBtn toggle
-const moreBtn = navItems[navItems.length - 1];
+//Developer Communities
+const itemDevComDetail = document.querySelector("#item_developer_communities_detail");
+const itemDevCom = itemDevComDetail.previousElementSibling;
+const itemDevComTriangleIcon = itemDevCom.querySelector("img");
+itemDevComDetail.addEventListener("mouseenter", () => {
+  itemDevCom.classList.toggle("item_extending");
+  itemDevComTriangleIcon.classList.toggle("triangle_icon_rotation");
+});
+itemDevComDetail.addEventListener("mouseleave", () => {
+  itemDevCom.classList.toggle("item_extending");
+  itemDevComTriangleIcon.classList.toggle("triangle_icon_rotation");
+});
+
+//moreBtn
+const moreBtn = document.querySelector("#items ul li:last-child");
+const itemDeveloperProfile = moreBtn.previousElementSibling;
+
 moreBtn.addEventListener("mouseenter", () => {
-  const listNotShowing = [];
-  navItems.forEach((list, index) => {
-    if (index !== navItems.length - 1) {
-      if (!list.classList.contains("showing")) {
-        listNotShowing.push(list.textContent);
-      }
-    }
-  });
+  itemDeveloperProfile.classList.toggle("item_extending");
+});
+
+moreBtn.addEventListener("mouseleave", () => {
+  itemDeveloperProfile.classList.toggle("item_extending");
+});
+
+itemDeveloperProfile.addEventListener("mouseenter", () => {
+  itemDeveloperProfile.classList.toggle("item_extending");
+});
+
+itemDeveloperProfile.addEventListener("mouseleave", () => {
+  itemDeveloperProfile.classList.toggle("item_extending");
+});
+
+/* developer profile circle */
+const developerProfileCircle = document.querySelector("#developer_profile");
+const developerProfileCircleDetail = developerProfileCircle.nextElementSibling;
+const developerProfileCircleClosePanel = document.querySelector("#developer_profile_close_panel");
+
+developerProfileCircle.addEventListener("click", () => {
+  developerProfileCircleDetail.classList.toggle("item_extending");
+});
+
+developerProfileCircleClosePanel.addEventListener("click", () => {
+  developerProfileCircleDetail.classList.toggle("item_extending");
 });
