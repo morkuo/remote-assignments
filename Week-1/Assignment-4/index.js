421e6b56657e56ef691cd210d4f6c66b6ca85eba
morkuo <80438073+morkuo@users.noreply.github.com>
2022-06-12 16:10:05 +0800

    use css to rotate navbar triangle icon. add helper function to better organize dynamic drop-down menus

--- Week-1/Assignment-4/index.js
+++ Week-1/Assignment-4/index.js
@@ -1,95 +1,21 @@
-// rotate triangle icons
+function addClassOnEvent(event, fireElement, styleElement, helperClass) {
+  fireElement.addEventListener(event, () => {
+    styleElement.classList.toggle(helperClass);
+  });
+}
+
+/*
+ * extends A element when B elemenet is hovered over or clicked
+ */
+
+//items
 const navItems = document.querySelectorAll("#items li");
 navItems.forEach(list => {
   const triangleIcon = list.querySelector(".triangle_icon");
   if (triangleIcon) {
-    list.addEventListener("mouseenter", () => {
-      triangleIcon.classList.toggle("triangle_icon_rotation");
-    });
-    list.addEventListener("mouseleave", () => {
-      triangleIcon.classList.toggle("triangle_icon_rotation");
-    });
+    addClassOnEvent("mouseenter", list, list, "item_extending");
+    addClassOnEvent("mouseleave", list, list, "item_extending");
   }
-});
-
-//Change color when the element is hovered over
-//language btns
-const languageToggleBtns = document.querySelectorAll(".language_toggle_btn");
-languageToggleBtns.forEach(btn => {
-  btn.addEventListener("mouseenter", () => {
-    btn.style.backgroundColor = "#f0f1f4";
-  });
-
-  btn.addEventListener("mouseleave", () => {
-    btn.style.backgroundColor = "white";
-  });
-});
-
-//developer profile
-const developerProfile = document.querySelector("#developer_profile");
-developerProfile.addEventListener("mouseenter", () => {
-  developerProfile.style.backgroundColor = "#f0f1f4";
-});
-
-developerProfile.addEventListener("mouseleave", () => {
-  developerProfile.style.backgroundColor = "white";
-});
-
-//account profile
-const accountProfile = document.querySelector("#account_profile");
-accountProfile.addEventListener("mouseenter", () => {
-  accountProfile.style.border = "2px solid rgba(174, 197, 243, 0.941)";
-});
-
-accountProfile.addEventListener("mouseleave", () => {
-  accountProfile.style.border = "none";
-});
-
-// extend search bar when it is clicked
-const searchBar = document.querySelector("#search_bar");
-const searchBarInputField = document.querySelector("#search_bar_input");
-const searchBarClosePanel = document.querySelector("#search_bar_close_panel");
-searchBarInputField.addEventListener("click", () => {
-  searchBar.classList.toggle("search_bar_extending");
-});
-
-searchBarClosePanel.addEventListener("click", () => {
-  searchBar.classList.toggle("search_bar_extending");
-});
-
-/* extends items' menu when it is hovered over */
-//items
-navItems.forEach(list => {
-  const triangleIcon = list.querySelector(".triangle_icon");
-  if (triangleIcon) {
-    list.addEventListener("mouseenter", () => {
-      list.classList.toggle("item_extending");
-    });
-    list.addEventListener("mouseleave", () => {
-      list.classList.toggle("item_extending");
-    });
-  }
-});
-
-//language bar
-const navLanguageClosePanel = document.querySelector("#nav_language_close_panel");
-const footerLanguageClosePanel = document.querySelector("#footer_language_close_panel");
-languageToggleBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.classList.toggle("item_extending");
-  });
-});
-
-navLanguageClosePanel.addEventListener("click", () => {
-  const extendingBtn = document.querySelector("#nav_language");
-  extendingBtn.classList.toggle("item_extending");
-  navLanguageClosePanel.classList.toggle("item_extending");
-});
-
-footerLanguageClosePanel.addEventListener("click", e => {
-  const extendingBtn = document.querySelector("#footer_language");
-  extendingBtn.classList.toggle("item_extending");
-  footerLanguageClosePanel.classList.toggle("item_extending");
 });
 
 //Hover展開內容自己維持自己展開
@@ -97,57 +23,55 @@ footerLanguageClosePanel.addEventListener("click", e => {
 const itemProductDetail = document.querySelector("#item_product_detail");
 const itemProduct = itemProductDetail.previousElementSibling;
 const itemProductTriangleIcon = itemProduct.querySelector("img");
-itemProductDetail.addEventListener("mouseenter", () => {
-  itemProduct.classList.toggle("item_extending");
-  itemProductTriangleIcon.classList.toggle("triangle_icon_rotation");
-});
-itemProductDetail.addEventListener("mouseleave", () => {
-  itemProduct.classList.toggle("item_extending");
-  itemProductTriangleIcon.classList.toggle("triangle_icon_rotation");
-});
+
+addClassOnEvent("mouseenter", itemProductDetail, itemProduct, "item_extending");
+addClassOnEvent("mouseleave", itemProductDetail, itemProduct, "item_extending");
 
 //Developer Communities
 const itemDevComDetail = document.querySelector("#item_developer_communities_detail");
 const itemDevCom = itemDevComDetail.previousElementSibling;
 const itemDevComTriangleIcon = itemDevCom.querySelector("img");
-itemDevComDetail.addEventListener("mouseenter", () => {
-  itemDevCom.classList.toggle("item_extending");
-  itemDevComTriangleIcon.classList.toggle("triangle_icon_rotation");
-});
-itemDevComDetail.addEventListener("mouseleave", () => {
-  itemDevCom.classList.toggle("item_extending");
-  itemDevComTriangleIcon.classList.toggle("triangle_icon_rotation");
-});
+
+addClassOnEvent("mouseenter", itemDevComDetail, itemDevCom, "item_extending");
+addClassOnEvent("mouseleave", itemDevComDetail, itemDevCom, "item_extending");
 
 //moreBtn
 const moreBtn = document.querySelector("#items ul li:last-child");
 const itemDeveloperProfile = moreBtn.previousElementSibling;
+const moreBtnTriangleIcon = moreBtn.querySelector("img");
 
-moreBtn.addEventListener("mouseenter", () => {
-  itemDeveloperProfile.classList.toggle("item_extending");
-});
+addClassOnEvent("mouseenter", moreBtn, itemDeveloperProfile, "item_extending");
+addClassOnEvent("mouseleave", moreBtn, itemDeveloperProfile, "item_extending");
+addClassOnEvent("mouseleave", itemDeveloperProfile, itemDeveloperProfile, "item_extending");
 
-moreBtn.addEventListener("mouseleave", () => {
-  itemDeveloperProfile.classList.toggle("item_extending");
-});
+//click 展開內容保持展開狀態，點擊原按鈕或按鈕外元素才關閉
+// search bar
+const searchBar = document.querySelector("#search_bar");
+const searchBarInputField = document.querySelector("#search_bar_input");
+const searchBarClosePanel = document.querySelector("#search_bar_close_panel");
 
-itemDeveloperProfile.addEventListener("mouseenter", () => {
-  itemDeveloperProfile.classList.toggle("item_extending");
-});
+addClassOnEvent("click", searchBarInputField, searchBar, "search_bar_extending");
+addClassOnEvent("click", searchBarClosePanel, searchBar, "search_bar_extending");
 
-itemDeveloperProfile.addEventListener("mouseleave", () => {
-  itemDeveloperProfile.classList.toggle("item_extending");
-});
+//language btn
+const languageToggleBtns = document.querySelectorAll(".language_toggle_btn");
+const navLanguageClosePanel = document.querySelector("#nav_language_close_panel");
+const footerLanguageClosePanel = document.querySelector("#footer_language_close_panel");
+const navLanguageBtn = document.querySelector("#nav_language");
+const footerLanguageBtn = document.querySelector("#footer_language");
 
-/* developer profile circle */
+languageToggleBtns.forEach(btn => addClassOnEvent("click", btn, btn, "item_extending"));
+
+addClassOnEvent("click", navLanguageClosePanel, navLanguageBtn, "item_extending");
+addClassOnEvent("click", navLanguageClosePanel, navLanguageClosePanel, "item_extending");
+
+addClassOnEvent("click", footerLanguageClosePanel, footerLanguageBtn, "item_extending");
+addClassOnEvent("click", footerLanguageClosePanel, footerLanguageClosePanel, "item_extending");
+
+// developer profile circle
 const developerProfileCircle = document.querySelector("#developer_profile");
 const developerProfileCircleDetail = developerProfileCircle.nextElementSibling;
 const developerProfileCircleClosePanel = document.querySelector("#developer_profile_close_panel");
 
-developerProfileCircle.addEventListener("click", () => {
-  developerProfileCircleDetail.classList.toggle("item_extending");
-});
-
-developerProfileCircleClosePanel.addEventListener("click", () => {
-  developerProfileCircleDetail.classList.toggle("item_extending");
-});
+addClassOnEvent("click", developerProfileCircle, developerProfileCircleDetail, "item_extending");
+addClassOnEvent("click", developerProfileCircleClosePanel, developerProfileCircleDetail, "item_extending");
