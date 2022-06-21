3ea4d3157189e2fb6e78e6c0a29236b68f7590cb
Morton Kuo <menghsinkuo@gmail.com>
2022-06-21 16:02:33 +0800

    improvement

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,56 +1,59 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(cookieParser());
 
+//Assignment 1
+app.get('/', (req, res) => {
+    res.send('Hello, My Server!');
+});
+
+//Assignment 2
 function sumToCertainNum(num) {
-  let sum = 0;
-  for (let i = num; i > 0; i--) {
-    sum += i;
-  }
-  return sum;
+    let sum = 0;
+    for (let i = num; i > 0; i--) {
+        sum += i;
+    }
+    return sum;
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello, My Server!");
+app.get('/data', (req, res) => {
+    let { number } = req.query;
+
+    if (!number) return res.send('Lack of Parameter');
+
+    number = +number;
+
+    if (!Number.isInteger(number) || Math.abs(number) != number)
+        return res.send('Wrong Parameter');
+
+    res.send(`${sumToCertainNum(number)}`);
 });
 
-app.get("/data", (req, res) => {
-  let { number } = req.query;
+//Assignment 4
+app.get('/myName', (req, res) => {
+    const username = req.cookies.username;
 
-  if (number === "") return res.send("Wrong Parameter");
-  if (!number) return res.send("Lack of Parameter");
-
-  number = +number;
-  if (!Number.isInteger(number) || Math.abs(number) != number)
-    return res.send("Wrong Parameter");
-
-  res.send(`${sumToCertainNum(+number)}`);
-});
-
-app.get("/myName", (req, res) => {
-  const username = req.cookies.username;
-
-  if (username) res.send(`${username}`);
-  else {
-    const form = `
+    if (username) res.send(`${username}`);
+    else {
+        const form = `
     <form action="/trackName">
       <p>Please enter your name</p>
       <input type="text" name="name">
       <button>Submit</button>
     </form>`;
 
-    res.send(form);
-  }
+        res.send(form);
+    }
 });
 
-app.get("/trackName", (req, res) => {
-  res.cookie("username", req.query.name);
-  res.redirect("/myName");
+app.get('/trackName', (req, res) => {
+    res.cookie('username', req.query.name);
+    res.redirect('/myName');
 });
 
 app.listen(3000, (req, res) => {
-  console.log("Server is running on port 3000");
+    console.log('Server is running on port 3000');
 });
