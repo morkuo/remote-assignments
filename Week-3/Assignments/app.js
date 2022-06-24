0fe2122fff13f659593421f2b5f8d56b96cf902c
Morton Kuo <menghsinkuo@gmail.com>
2022-06-25 00:31:55 +0800

    improve efficiency of sumToCertainNum function

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -7,53 +7,58 @@ app.use(cookieParser());
 
 //Assignment 1
 app.get('/', (req, res) => {
-    res.send('Hello, My Server!');
+  res.send('Hello, My Server!');
 });
 
 //Assignment 2
-function sumToCertainNum(num) {
-    let sum = 0;
-    for (let i = num; i > 0; i--) {
-        sum += i;
-    }
-    return sum;
+/*
+function sumToCertainNumLinearTime(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+*/
+
+function sumToCertainNumConstantTime(num) {
+  return ((num + 1) * num) / 2;
 }
 
 app.get('/data', (req, res) => {
-    let { number } = req.query;
+  let { number } = req.query;
 
-    if (!number) return res.send('Lack of Parameter');
+  if (!number) return res.send('Lack of Parameter');
 
-    number = +number;
+  number = +number;
 
-    if (!Number.isInteger(number) || Math.abs(number) != number)
-        return res.send('Wrong Parameter');
+  if (!Number.isInteger(number) || Math.abs(number) != number) return res.send('Wrong Parameter');
 
-    res.send(`${sumToCertainNum(number)}`);
+  res.send(`${sumToCertainNumConstantTime(number)}`);
 });
 
 //Assignment 4
 app.get('/myName', (req, res) => {
-    const username = req.cookies.username;
+  const username = req.cookies.username;
 
-    if (username) res.send(`${username}`);
-    else {
-        const form = `
+  if (username) res.send(`${username}`);
+  else {
+    const form = `
     <form action="/trackName">
       <p>Please enter your name</p>
       <input type="text" name="name">
       <button>Submit</button>
     </form>`;
 
-        res.send(form);
-    }
+    res.send(form);
+  }
 });
 
 app.get('/trackName', (req, res) => {
-    res.cookie('username', req.query.name);
-    res.redirect('/myName');
+  res.cookie('username', req.query.name);
+  res.redirect('/myName');
 });
 
 app.listen(3000, (req, res) => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
