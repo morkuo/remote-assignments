8a7dc91141b16a7a7c2b56469b2d7545c3ed1560
Morton Kuo <menghsinkuo@gmail.com>
2022-06-29 10:18:21 +0800

    submit assignment-4

---
+++ Week-4/Assignment-4/app.js
@@ -0,0 +1,16 @@
+function delayedResultPromise(n1, n2, delayTime) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(n1 + n2);
+    }, delayTime);
+  });
+}
+
+delayedResultPromise(4, 5, 3000).then(console.log);
+
+async function main() {
+  const result = await delayedResultPromise(4, 5, 3000);
+  console.log(result);
+}
+
+main();
